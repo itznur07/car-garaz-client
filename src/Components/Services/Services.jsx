824becb121d34ee98813c94514b954df31ca2ab5
Md Nuruddin <nuruddinalways99@gmail.com>
2023-05-12 12:36:30 +0600

    Services component

---
+++ src/Components/Services/Services.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { FaArrowRight } from "react-icons/fa";
+import servicesData from "../../../public/services.json";
+
+const Services = () => {
+  return (
+    <div className='mx-14 my-14'>
+      <div className='text-center'>
+        <h2 className='text-3xl font-bold text-[#ff3811] mb-2'>Services</h2>
+        <h3 className='text-5xl font-bold  mb-4'>Our Service Area</h3>
+        <p className='max-w-3xl mx-auto text-gray-500 mb-8'>
+          The majority have suffered alteration in some form, by injected
+          humour, or randomised words which don't look even slightly believable.
+        </p>
+      </div>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
+        {servicesData.map((service) => (
+          <div
+            key={service.id}
+            className='bg-white rounded-lg shadow-md overflow-hidden'
+          >
+            <div className='relative'>
+              <img
+                src={service.img}
+                alt={service.title}
+                className='w-full h-64 object-cover object-center'
+              />
+            </div>
+            <div className='px-4 py-6'>
+              <div>
+                <h4 className='text-2xl font-bold'>{service.title}</h4>
+              </div>
+              <div className='flex justify-between my-5'>
+                <span className='text-xl font-bold text-[#ff3811]'>
+                  Price: {service.price}
+                </span>
+                <FaArrowRight className='h-6 w-6 text-[#ff3811]' />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className='text-center mt-12'>
+        <button className='border border-[#ff3811] text-[#ff3811] px-6 py-3 rounded-md  transition-all duration-300 font-bold'>
+          More Services
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import About from "../../Components/About/About";
 import Banner from "../../Components/Banner/Banner";
+import Services from "../../Components/Services/Services";
 
 const Home = () => {
   return (
     <>
       <Banner></Banner>
       <About></About>
+      <Services></Services>
     </>
   );
 };
